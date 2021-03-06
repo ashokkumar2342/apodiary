720b5a0b0c2070de1ea4745e80ec41168c126f5c
ashokkumar2342 <ashokkumar2342@gmail.com>
2021-03-06 16:33:56 +0530

    first commit

---
+++ android/app/src/main/java/com/apodiary/MainActivity.java
@@ -0,0 +1,15 @@
+package com.apodiary;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "apodiary";
+  }
+}
